aee6b41769849e333b849e1ba2ce5fb8d8afd7b1
Ondřej Palovský <zarremgregarrok@gmail.com>
2024-09-19 07:31:06 +0200

    Part two

--- src/main/java/com/commerzbank/Functions.java
+++ src/main/java/com/commerzbank/Functions.java
@@ -5,4 +5,26 @@ public class Functions {
     static boolean NAND(boolean A, boolean B) {
         return !(A && B);
     }
+
+    private static boolean NOT(boolean A) {
+        return NAND(A, A);
+    }
+
+    static boolean AND(boolean A, boolean B) {
+        return NOT(NAND(A, B));
+    }
+
+    static boolean OR(boolean A, boolean B) {
+        return NAND(NOT(A), NOT(B));
+    }
+
+    static boolean NOR(boolean A, boolean B) {
+        return NOT(OR(A, B));
+    }
+
+
+    static boolean XOR(boolean A, boolean B) {
+        boolean C = NAND(A, B);
+        return NAND(NAND(A, C), NAND(C, B));
+    }
 }
--- src/test/groovy/com/commerzbank/FunctionsTest.groovy
+++ src/test/groovy/com/commerzbank/FunctionsTest.groovy
@@ -4,13 +4,71 @@ package com.commerzbank
 import spock.lang.Specification
 
 class FunctionsTest extends Specification {
-    def "NAND logic"(boolean A, boolean B, boolean Q) {
+    def "NAND gate"(boolean A, boolean B, boolean Q) {
         expect:
         Functions.NAND(A, B) == Q
 
         where:
         A | B | Q
         0 | 0 | 1
+        0 | 1 | 1
+        1 | 0 | 1
+        1 | 1 | 0
+    }
+
+    @SuppressWarnings('GroovyAccessibility')
+    def "NOT gate"(boolean A, boolean Q) {
+        expect:
+        Functions.NOT(A) == Q
+        where:
+        A | Q
+        0 | 1
+        1 | 0
+    }
+
+    def "NOR gate"(boolean A, boolean B, boolean Q) {
+        expect:
+        Functions.NOR(A, B) == Q
+
+        where:
+        A | B | Q
+        0 | 0 | 1
+        0 | 1 | 0
+        1 | 0 | 0
+        1 | 1 | 0
+    }
+
+    def "AND gate"(boolean A, boolean B, boolean Q) {
+        expect:
+        Functions.AND(A, B) == Q
+
+        where:
+        A | B | Q
+        0 | 0 | 0
+        0 | 1 | 0
+        1 | 0 | 0
+        1 | 1 | 1
+    }
+
+    def "OR gate"(boolean A, boolean B, boolean Q) {
+        expect:
+        Functions.OR(A, B) == Q
+
+        where:
+        A | B | Q
+        0 | 0 | 0
+        0 | 1 | 1
+        1 | 0 | 1
+        1 | 1 | 1
+    }
+
+    def "XOR gate"(boolean A, boolean B, boolean Q) {
+        expect:
+        Functions.XOR(A, B) == Q
+
+        where:
+        A | B | Q
+        0 | 0 | 0
         0 | 1 | 1
         1 | 0 | 1
         1 | 1 | 0
